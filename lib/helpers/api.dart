c0460552ccf92115c80f62ff706f2bc96edc93ff
Jun Young Bang <bhang96@gmail.com>
2021-10-25 15:51:28 +0800

    Separate recurring codes pattern in GraphQl Queries to fragments (

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,73 +1,143 @@
-class GraphQlQuery{
+class GraphQlFragment {
+  static final jobsTypesFields = '''
+    fragment jobTypesFields on JobType {
+      category
+      name
+    }
+  ''';
+
+  static final jobsWorkingHourFields = '''
+    fragment jobsWorkingHourFields on WorkingHour {
+      _id
+      day_of_week
+      end_time
+      shift_name
+      start_time
+    }
+  ''';
+
+  static final jobFields = '''
+    ${GraphQlFragment.jobsWorkingHourFields}
+    
+    fragment jobFields on Job {
+        job_name
+        _created_at
+        _id
+        company {
+          name
+          about
+        }
+        is_saved
+        attributes {
+          category
+          category_display_sequence
+        }
+        allowances {
+          name
+          description
+        }
+        job_types{
+          category
+          name
+        }
+        to_monthly_rate
+        to_hourly_rate
+        education_requirement{
+          category
+          level
+          name
+        }
+        employment
+        employment_type {
+          name
+        }
+        state
+        attributes {
+          category
+        }
+        address{
+          address
+          formatted_address
+        }
+        address_on_map
+        images
+        to_working_days_per_week
+        to_working_hours_per_day
+        vacancy
+        working_hour{
+          ...jobsWorkingHourFields
+        }
+        spoken_skill {
+          level
+          name
+        }
+        written_skill {
+          level
+          name
+        }
+    }
+  ''';
+
+  static final jobCardFields = '''
+    ${GraphQlFragment.jobsWorkingHourFields}
+
+    fragment jobFields on Job {
+        job_name
+        _created_at
+        _id
+        company {
+          name
+          about
+        }
+        is_saved
+        attributes {
+          category
+          category_display_sequence
+        }
+        allowances {
+          name
+          description
+        }
+        job_types{
+          category
+          name
+        }
+        employment
+        to_monthly_rate
+        to_hourly_rate
+        employment_type {
+          name
+        }
+        state
+        attributes {
+          category
+        }
+        address{
+          address
+          formatted_address
+        }
+        address_on_map
+        images
+        start_date
+        end_date    
+        to_working_days_per_week
+        to_working_hours_per_day
+        vacancy
+        working_hour{
+         ...jobsWorkingHourFields
+        }
+    }
+  ''';
+}
+
+class GraphQlQuery {
   static String getJob() {
     return """
+    ${GraphQlFragment.jobFields}
+    
     query job(\$ids: [ID]) {
-    get_jobs(_id: \$ids){
-      _id
-      _created_at
-      job_name
-      start_date
-      end_date
-      company  {
-        name
-        about
-      }
-      is_saved
-      education_requirement{
-        category
-        level
-        name
-      }
-      spoken_skill{
-        name
-        level
-      }
-      written_skill{
-        name
-        level
-      }
-      attributes {
-        category
-        category_display_sequence
-      }
-      allowances {
-        name
-        description
-      }
-      job_types {
-        category
-        name
-        __typename
-      }
-      to_monthly_rate
-      to_hourly_rate
-
-      employment
-      employment_type {
-        name
-      }
-      state
-      attributes {
-        category
-      }
-      address {
-        address 
-        formatted_address
-      }
-      address_on_map
-      images
-      is_applied
-      is_invited
-      to_working_days_per_week
-      to_working_hours_per_day
-      vacancy
-      working_hour{
-        _id
-        day_of_week
-        end_time
-        shift_name
-        start_time
-        }
+      get_jobs(_id: \$ids){
+      ...jobFields
       }
     }
     """;
@@ -75,75 +145,17 @@ class GraphQlQuery{
 
   static String getAllJobs() {
     return """
+    ${GraphQlFragment.jobFields}
+
     query job (\$limit: Int){
       job_search (limit: \$limit){
         total
         result{
-          _id
-          _created_at
-          job_name
-          company {
-            name
-            about
-          }
-          attributes {
-            category
-            category_display_sequence
-          }
-          allowances {
-            name
-            description
-          }
-          job_types {
-            category
-            name
-            __typename
-          }
-          education_requirement{
-            category
-            level
-            name
-          }
-          to_monthly_rate
-          to_hourly_rate
-    
-          employment
-          employment_type {
-            name
-          }
-         spoken_skill{
-            name
-            level
-          }
-          written_skill{
-            name
-            level
-          }
-          state
-          attributes {
-            category
-          }
-          address{
-            address
-            formatted_address
-          }
-          address_on_map
-          images
-          to_working_days_per_week
-          to_working_hours_per_day
-          vacancy
-          working_hour{
-            _id
-            day_of_week
-            end_time
-            shift_name
-            start_time
-          }
+          ...jobFields
         }
       }
     }
   """;
-
   }
 
   static String updateBookmark() {
@@ -166,17 +178,17 @@ class GraphQlQuery{
 
   static String getBookmarks() {
     return """
-      query SavedJob(\$limit){
+
+      query SavedJob(\$limit: Int){
         saved_jobs(limit:\$limit) {
           total
           bookmarks {
-            _created_at
             job {
-              _id
+               _id
               _created_at
               job_name
-              start_date
-              end_date
+                start_date
+                end_date
               address{
                 address
                 formatted_address
@@ -188,18 +200,16 @@ class GraphQlQuery{
               job_types {
                 category
                 name
-                __typename
               }
               to_monthly_rate
               to_hourly_rate
-        
+            
               employment
               employment_type {
                 name
               }
             }
           }
-          
         }
       }
     """;
@@ -207,52 +217,13 @@ class GraphQlQuery{
 
   static String SearchWithParams() {
     return """
+      ${GraphQlFragment.jobFields}
+
       query SearchWithParams(\$limit: Int, \$monthly_rate: [Float], \$term: String) {
         job_search(limit: \$limit, monthly_rate: \$monthly_rate, term: \$term) {
           total
           result {
-            _id
-            _created_at
-            job_name
-            company {
-              name
-              about
-            }
-            attributes {
-              category
-              category_display_sequence
-            }
-            allowances {
-              name
-              description
-            }
-            education_requirement {
-              category
-              level
-              name
-            }
-            job_types {
-              _id
-              category
-              name
-              __typename
-            }
-            to_monthly_rate
-            to_hourly_rate
-            employment
-            employment_type {
-              name
-            }
-            state
-            attributes {
-              category
-            }
-            address {
-              address
-              formatted_address
-            }
-            address_on_map
-            images
+            ...jobFields
           }
         }
       }
